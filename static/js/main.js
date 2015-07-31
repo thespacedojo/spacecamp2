9c3857a127e1f4e258ef1b3bce2a862ff9955800
Josh Owens <joshua.owens@gmail.com>
2015-07-31 13:19:51 -0400

    Get started on static site for spacecamp

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+// 	console.log("Hello!");
+//
+// 	var el = $('.triangles').width() / 2;
+// 	$('.left-triangle').css('border-width', "100px 0 0 " + el + "px");
+// 	$('.right-triangle').css('border-width', "100px " + el + "px 0 0");
+// });
+
+// TODO: add scroll to bottom of page (get ticket button)
